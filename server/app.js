00b32d10025610c278b4eb570f2b96041e4cc458
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2022-05-13 20:43:13 +0100

    Created CSV file from artist.search results

--- server/app.js
+++ server/app.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const fs = require("fs");
 
+const backupfetchArtists = async () => {
+  try {
+    let result;
+    await axios
+      .get(
+        `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=30d82bc1657cfc2bb5cf2b2af64aac2c&format=json`
+      )
+      .then((response) => {
+        result = response.data.artists.artist.map((artist) => {
+          return artist.name;
+        });
+        const sampleObject = { result };
 
-
-
+        fs.writeFile(
+          "./object.json",
+          JSON.stringify(sampleObject, null, 4),
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log("File has been created");
+          }
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const fetchArtists = async (searchValue) => {
   try {
@@ -25,7 +56,20 @@ const fetchArtists = async (searchValue) => {
   }
 };
 
+app.get("/api/backup", async (req, res, next) => {
+  try {
+    const { page } = req.query;
 
+    const data = await backupfetchArtists();
+    console.log("data", data);
+    return res.status(200).json({
+      status: 200,
+      data,
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.get("/api/:searchValue", async (req, res, next) => {
   try {
